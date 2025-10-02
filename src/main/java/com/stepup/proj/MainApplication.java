b771420d4a33a9c60e547fa352b39627ae1afe44
Maiia Borinos <maya.borinos@gmail.com>
2025-10-02 22:21:38 +0400

    Доработки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,7 +3,9 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("111111111");
+        System.out.println("100011");
+        System.out.println("2222");
+        System.out.println("3333");
 
     }
 }
